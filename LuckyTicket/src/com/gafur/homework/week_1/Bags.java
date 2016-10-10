e14345305110d5d8104fdc045d4fd793586a242b
Gafurov Ivan <Gafurov0ivan@gmail.com>
2016-10-10 15:37:43 +0300

    Step2

--- LuckyTicket/src/com/gafur/homework/week_1/Bags.java
+++ LuckyTicket/src/com/gafur/homework/week_1/Bags.java
@@ -7,26 +7,20 @@ public class Bags {
 
 	public static void main(String[] args) {
 
-		int current = differencePair(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9]);
-		best(current);
-		System.out.println(best);
-	}
+		for (int i1 = 0; i1 < a.length; i1++) {
+			for (int i2 = 0; i2 < a.length; i2++) {
+				for (int i3 = 0; i3 < a.length; i3++) {
+					for (int i4 = 0; i4 < a.length; i4++) {
+						for (int i5 = 0; i5 < a.length; i5++) {
+							for (int i6 = 0; i6 < a.length; i6++) {
+								for (int i7 = 0; i7 < a.length; i7++) {
+									for (int i8 = 0; i8 < a.length; i8++) {
+										for (int i9 = 0; i9 < a.length; i9++) {
+											for (int i10 = 0; i10 < a.length; i10++) {
+												int current = differencePair(a[i1], a[i2], a[i3], a[i4], a[i5], a[i6],
+														a[i7], a[i8], a[i9], a[i10]);
 
-	public static int differencePair(int a, int b, int c, int d, int e, int f, int g, int k, int n, int m) {
-		int difference = 0;
-
-		for (int i1 = 0; i1 < 10; i1++) {
-			for (int i2 = 0; i2 < 10; i2++) {
-				for (int i3 = 0; i3 < 10; i3++) {
-					for (int i4 = 0; i4 < 10; i4++) {
-						for (int i5 = 0; i5 < 10; i5++) {
-							for (int i6 = 0; i6 < 10; i6++) {
-								for (int i7 = 0; i7 < 10; i7++) {
-									for (int i8 = 0; i8 < 10; i8++) {
-										for (int i9 = 0; i9 < 10; i9++) {
-											for (int i10 = 0; i10 < 10; i10++) {
-
-												difference = (a + b + c + d + e) - (f + g + k + n + m);
+												best(current);
 
 											}
 										}
@@ -39,13 +33,18 @@ public class Bags {
 			}
 		}
 
+		System.out.println(best);
+	}
+
+	public static int differencePair(int a, int b, int c, int d, int e, int f, int g, int k, int n, int m) {
+		int difference = (a + b + c + d + e) - (f + g + k + n + m);
 		return difference;
 	}
 
 	public static int best(int current) {
 
 		if (best < 0) {
-			best = 0;
+			best=0;
 		}
 		if (best > current) {
 			best = current;

f363d2b111cb4d23b93184a51d5805d9492dcd59
Gafurov0ivan <Gafurov0ivan@gmail.com>
2016-10-12 22:33:39 +0300

    Step5

--- LuckyTicket/src/com/gafur/homework/week_1/MergeArray.java
+++ LuckyTicket/src/com/gafur/homework/week_1/MergeArray.java
@@ -1,59 +1,42 @@
 package com.gafur.homework.week_1;
 
 public class MergeArray {
-
-	private static int x;
-
-
 	public static void main(String[] args) {
 		int[] arrayfirst = { 0, 3, 4, 7, 10, 11, 20 };
 		int[] arraySecond = { -5, -1, 10, 31, 55, 73, 357, 457 };
-		
-		int[] arraySort = mergeSort(0,0, arrayfirst, arraySecond);
-		
-		for(int item : arraySort)
-		System.out.println(item);
+		int[] arraySort = mergeSort(arrayfirst, arraySecond);
+
+		for (int item : arraySort)
+			System.out.println(item);
 	}
 
+	public static int[] mergeSort(int[] arrayFirst, int[] arraySecond) {
+		int[] thirdArray = new int[arrayFirst.length + arraySecond.length];
+		int firstCapacity = 0;
+		int secondCapacity = 0;
+		for (int i = 0; i < thirdArray.length; i++) {
+			if (firstCapacity < arrayFirst.length && secondCapacity < arraySecond.length) {
+				if (arrayFirst[firstCapacity] <= arraySecond[secondCapacity]) {
+					thirdArray[i] = arrayFirst[firstCapacity];
+					firstCapacity++;
+				}
 
-//			for (int j = 0, i = 0; j < firstArray.length; j++, i++) {
-//				if(i == secondArray.length){
-//					break;
-//				}
-//				if (secondArray[i] <= firstArray[j]) {
-//					bufer = secondArray[i];
-//				} else {
-//					bufer = firstArray[j];
-//				}
-//				
-//				}
-//				
-//			}
-//		
-//
-//		for (int out : thirdArray) {
-//			System.out.println(out);
-//		}
-//	}
-	
-	public static int[] mergeSort(int i, int j, int[] arrayfirst, int[] arraySecond){
-		int[] thirdArray = new int[arrayfirst.length + arraySecond.length];
-		if(arrayfirst[i] <= arraySecond[j]){
-			if(x<thirdArray.length){
-				thirdArray[x] = arrayfirst[i];
-				x++;
-		}
-			i++;
+				else {
+					thirdArray[i] = arraySecond[secondCapacity];
+					secondCapacity++;
+				}
+			} else {
+				if (firstCapacity == arrayFirst.length && secondCapacity < arraySecond.length) {
+					thirdArray[i] = arraySecond[secondCapacity];
+					secondCapacity++;
+				}
+				if (secondCapacity == arraySecond.length && firstCapacity < arrayFirst.length) {
+					thirdArray[i] = arrayFirst[firstCapacity];
+					firstCapacity++;
+				}
+			}
 
-		}
-		else{
-			if(x<thirdArray.length){
-				thirdArray[x] = arraySecond[j];
-				x++;
-		}
-			mergeSort(i+1, j+1, arrayfirst, arraySecond);
 		}
 		return thirdArray;
 	}
 }
-
---
+++ LuckyTicket/src/com/gafur/homework/week_1/MergeRecursion.java
@@ -0,0 +1,24 @@
+package com.gafur.homework.week_1;
+
+public class MergeRecursion {
+
+	public static void main(String[] args) {
+		
+
+	}
+//	public static int[] mergeSort(int[] array){
+//		int start = array[0];
+//		int end = array[array.length-1];
+//		
+//		int[] bufer = array/2;
+//		
+//		if(array.length == 1){
+//			return array;
+//		}
+//		if()
+//		
+//			
+//			mergeSort()
+//	}
+
+}

aa406fc4b1face19a17fb99b92fb9cd51f72653f
Gafurov0ivan <Gafurov0ivan@gmail.com>
2016-10-23 21:15:40 +0300

    Step24

--- LuckyTicket/src/com/gafur/homework/week_3/annotations/Tester.java
+++ LuckyTicket/src/com/gafur/homework/week_3/annotations/Tester.java
@@ -9,38 +9,35 @@ public class Tester {
 
 	public void testAll(String[] classes) {
 
-		try {
-			for (String item : classes) {
-				Method[] methods = item.getClass().getDeclaredMethods();
+		for (String item : classes) {
+			Method[] methods = item.getClass().getDeclaredMethods();
 
-				for (Method method : methods) {
-					Test test = method.getAnnotation(Test.class);
-					if (test != null) {
-						boolean answer = test.methodReturn();
-						
+			for (Method method : methods) {
+				Test test = method.getAnnotation(Test.class);
+				if (test != null) {
+					boolean answer = test.methodReturn();
+					try {
+
 						method.setAccessible(true);
-							Object v = method.get();
-							if (v instanceof String) {
-								stringValue = (String) v;
-							}
+						Class<?> c = Class.forName(item);
+						Object newObject = c.newInstance();
+						method.invoke(newObject, 15);
 
-							if(stringValue.length() < length){
-								return false;
-							}
-				
+					} catch (SecurityException e) {
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					} catch (InstantiationException e) {
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						e.printStackTrace();
+					} catch (IllegalArgumentException e) {
+						e.printStackTrace();
+					} catch (InvocationTargetException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
 		}
 	}
 }

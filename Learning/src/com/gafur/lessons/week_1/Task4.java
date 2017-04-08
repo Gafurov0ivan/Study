db9b6a02b56e0224832593ed81eeade3b851a396
Ivan <Gafurov0ivan@gmail.com>
2017-04-08 12:58:27 +0300

    Update Task4.java

--- Learning/src/com/gafur/lessons/week_1/Task4.java
+++ Learning/src/com/gafur/lessons/week_1/Task4.java
@@ -7,20 +7,16 @@ package com.gafur.lessons.week_1;
 public class Task4 {
 
     public static void main(String[] args) {
-        // ��������� ��� �����
         double d1 = 1.3425434545345d;
         double d2 = 1.3534565645656d;
-        // �� �����
         if (Math.abs(d1 - d2) <= 1e-8) {
         }
-        // �� ������, ������
         if (d1 - d2 > 1e-8) {
         }
         int i = 1;
         System.out.println(i / 2);
     }
 
-    //	��������
     public int rec(int i) {
         if (i == 0) {
             return 1;
